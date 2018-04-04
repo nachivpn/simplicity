08b153789d34c14df9e6b5f4c9cea35597ad0cfb
nachivpn <nachivpn@gmail.com>
2018-04-04 23:38:47 +0200

    adds all obj constraints

--- src/BCC.hs
+++ src/BCC.hs
@@ -10,20 +10,21 @@ data a :+: b   -- Coproducts
 
 {-- Morphisms --}
 data Mph obj a b where
-  -- Id, and composition (present in every category)
-    Id       :: obj a => Mph obj a a
-    O        :: (obj a, obj b, obj c) => Mph obj b c -> Mph obj a b -> Mph obj a c
+    -- Id, and composition (present in every category)
+    Id        :: obj a => Mph obj a a
+    O         :: (obj a, obj b, obj c) => Mph obj b c -> Mph obj a b -> Mph obj a c
     -- When having products
-    Factor   ::  (obj a, obj b1, obj b2) => Mph obj a b1 -> Mph obj a b2 -> Mph obj a (b1 :*: b2)
+    Factor    ::  (obj a, obj b1, obj b2, obj (b1 :*: b2)) => Mph obj a b1 -> Mph obj a b2 -> Mph obj a (b1 :*: b2)
     -- Pairs proyections
-    Fst      :: (obj a, obj b) => Mph obj (a :*: b) a
-    Snd      :: (obj a, obj b) => Mph obj (a :*: b) b
+    Fst       :: (obj a, obj b, obj (a :*: b)) => Mph obj (a :*: b) a
+    Snd       :: (obj b, obj b, obj (a :*: b)) => Mph obj (a :*: b) b
     -- Terminal
-    Terminal :: Mph r a T
+    Terminal  :: Mph obj a T
     -- Coproducts
-    Inj1 :: (obj a, obj b) => Mph obj a (a :+: b)
-    Inj2 :: (obj a, obj b) => Mph obj b (a :+: b)
-    Copair  :: (obj a, obj b, obj c, obj e) => Mph obj (e :*: a) c -> Mph obj (e :*: b) c -> Mph obj (e :*: (a :+: b)) c
+    Inj1      :: (obj a, obj b, obj (a :+: b)) => Mph obj a (a :+: b)
+    Inj2      :: (obj a, obj b, obj (a :+: b)) => Mph obj b (a :+: b)
+    Copair    :: (obj a, obj b, obj c, obj e, obj (e :*: a), obj (e :*: b), obj (a :+: b), obj (e :*: (a :+: b))) 
+              => Mph obj (e :*: a) c -> Mph obj (e :*: b) c -> Mph obj (e :*: (a :+: b)) c
 
 -- CCC to String
 toString :: (Mph obj a b) -> String
@@ -38,7 +39,7 @@ toString Inj2           = "Inj2"
 toString (Copair f g)   = "[" ++ toString f ++ ", " ++ toString g ++ "]"
 
 -- Products are symmetric wrt isomorphism
-prodFlip :: (obj a, obj e, obj (a :*: e)) => Mph obj (a :*: e) (e :*: a)
+prodFlip :: (obj a, obj e, obj (a :*: e), obj (e :*: a)) => Mph obj (a :*: e) (e :*: a)
 prodFlip = Factor Snd Fst
 
 
